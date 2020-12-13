aff9cd3c13c5a22df89c23dce4ea9b984abbbe54
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2020-12-13 13:40:29 +0100

    Add files via upload

---
+++ Pages/Students/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public DetailsModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var emptyStudent = new Student();
+
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                _context.Students.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
+    }
+}
